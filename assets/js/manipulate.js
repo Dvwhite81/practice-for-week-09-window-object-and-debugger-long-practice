47059b1a83bc57ed6d2e42a950e3f91b8fda123b
Dan <dvwhite1981@gmail.com>
2023-08-13 13:28:45 -0400

    Complete

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,21 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    document.title = "Dan White's Portfolio";
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    document.body.children[0].children[0].innerText = "I am Dan White";
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
     // Your code here
+    document.body.children[1].children[1].innerText = "This is a small passage about myself. I'm liking week 9 so far."
 }
